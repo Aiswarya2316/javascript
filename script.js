8b6828f0f2685b450004aa5fd2e597840f1baf1d
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-09-18 11:14:04 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+// var a
+// a=10
+// let a=10
+// a=20
+// const a=10
+// a=20
+// let,const
+// datatypes
+// let a="hai"
+// let a=true
+// let a
+// let a=null
+// let a=[1,2,3]
+// let a=12345678901234567890
+
+// operators
+// let a=10
+// let b=20
+
+// let a=20
+// let b=10
+
+// let a=20
+// let b=10
+
+// let a=30
+// a++
+
+// let a=20
+// a--
+
+
+// assignment
+// let b=20
+// b+=10
+// b-=10
+// b/=2
+// b%=5
+// b*=2
+alert(b)
+    
