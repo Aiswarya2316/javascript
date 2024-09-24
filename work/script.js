72a342c58bcf0a9e6d92e264dc539ba297a8b6fc
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-09-24 11:26:17 +0530

    age

---
+++ work/script.js
@@ -0,0 +1,6 @@
+d=[{name:'anu',age:21,place:'vkm'},{name:'anu',age:31,place:'vkm'},{name:'anu',age:41,place:'vkm'},
+    {name:'anu',age:51,place:'vkm'},{name:'anu',age:61,place:'vkm'}]
+
+    d.forEach(element =>{
+        console.log(element.age);
+    });
