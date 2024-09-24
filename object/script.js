d4d5cebe0d5ea19e63dcf66b942383a286833ecc
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-09-24 10:40:09 +0530

    age

---
+++ object/script.js
@@ -0,0 +1,28 @@
+// d={name:'anu',age:21,place:'vkm'}
+
+// console.log(d);
+// console.log(d['age'])
+// console.log(d.age)
+// console.log(d.place)
+
+
+// d.place='ekm'
+// d['mark']=25
+// d.lang='hindi'
+// console.log(d);
+
+// delete d.age
+// console.log(d);
+
+
+// for(let i in d){
+//     if(d['age']==21){
+//         console.log(i,d[i]);
+//     }
+// }
+
+
+
+
+
+
---
+++ task/script.js
@@ -0,0 +1,17 @@
+d=[{name:'anu',age:21,place:'vkm'},{name:'anu',age:31,place:'vkm'},{name:'anu',age:41,place:'vkm'},
+{name:'anu',age:51,place:'vkm'},{name:'anu',age:61,place:'vkm'}]
+
+above=[ ]
+below=[ ]
+
+for(let i of d){
+    if(i.age>=40){
+    above.push(i);
+}
+else{
+    below.push(i)
+}
+}
+
+console.log(above);
+console.log(below);
